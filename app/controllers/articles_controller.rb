e8e522dfd9e3e64cd3ddae0edde92b6c0ca0ec55
Arjit Singh <arjit1516@gmail.com>
2022-06-17 17:50:49 +0530

    Added one to many associations to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.user = User.first
         @article.save
         if @article.save
             flash[:notice] = "Article was created successfully."
