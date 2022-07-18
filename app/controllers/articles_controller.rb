870c4c48e5dd5e4522b0f528c9e78502cab639ba
Arjit Singh <arjit1516@gmail.com>
2022-07-18 16:48:35 +0530

    Alpha blog full complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(params.require(:article).permit(:title, :description, category_ids: []))
         @article.user = current_user
         if @article.save
             flash[:notice] = "Article was created successfully."
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
 
     def update
         @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(params.require(:article).permit(:title, :description, category_ids: []))
           flash[:notice] = "Article was updated successfully."
           redirect_to @article
         else
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
